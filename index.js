db7dc69e8892f94b45151566a141026b00510e0b
estrattonbailey <ericstrattonbailey@gmail.com>
2017-06-27 12:27:04 -0400

    Initial demo commit

---
+++ index.js
@@ -0,0 +1,76 @@
+import Popover from '../package/dist/index.js'
+
+window.addEventListener('DOMContentLoaded', e => {
+  const left = document.getElementById('popoverParentLeft')
+  const popLeft = new Popover({
+    target: left,
+    popover: `
+      <div class="my-popover left">
+        <h5 class="mv0">Left</h5>
+      </div>
+    `,
+    position: 'left'
+  })
+  left.addEventListener('mouseenter', e => {
+    popLeft.pin()
+  })
+  left.addEventListener('mouseleave', e => {
+    popLeft.unpin()
+  })
+  /**
+   * Make sure it closes on mouseleave,
+   * unlike others
+   */
+  popLeft.popover.addEventListener('mouseleave', popLeft.unpin)
+
+  const right = document.getElementById('popoverParentRight')
+  const popRight = new Popover({
+    target: right,
+    popover: `
+      <div class="my-popover right">
+        <h5 class="mv0">Right</h5>
+      </div>
+    `,
+    position: 'right'
+  })
+  right.addEventListener('mouseenter', e => {
+    popRight.pin()
+  })
+  right.addEventListener('mouseleave', e => {
+    popRight.unpin()
+  })
+
+  const top = document.getElementById('popoverParentTop')
+  const popTop = new Popover({
+    target: top,
+    popover: `
+      <div class="my-popover top">
+        <h5 class="mv0">Top</h5>
+      </div>
+    `,
+    position: 'top'
+  })
+  top.addEventListener('mouseenter', e => {
+    popTop.pin()
+  })
+  top.addEventListener('mouseleave', e => {
+    popTop.unpin()
+  })
+
+  const bottom = document.getElementById('popoverParentBottom')
+  const popBottom = new Popover({
+    target: bottom,
+    popover: `
+      <div class="my-popover bottom">
+        <h5 class="mv0">Bottom</h5>
+      </div>
+    `,
+    position: 'bottom'
+  })
+  bottom.addEventListener('mouseenter', e => {
+    popBottom.pin()
+  })
+  bottom.addEventListener('mouseleave', e => {
+    popBottom.unpin()
+  })
+})
